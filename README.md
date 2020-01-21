6ec2062645d1e23b0afc3599ef739132d722099c
Daniel Larsen <dalars@microsoft.com>
2020-01-21 15:59:45 +1300

    Added usage example with resource token (user permissions)

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Simple generic data access helper for Cosmos DB.
 
 See [`src/CosmosData.Tests/Usage.cs`](src/CosmosData.Tests/Usage.cs) for usage.
+
+## Resource Tokens / User Permissions
+
+See [`src/CosmosData.Tests/UserPermissions.cs`](src/CosmosData.Tests/UserPermissions.cs) for usage.
+
+Secure access to data in Azure Cosmos DB: <https://docs.microsoft.com/en-us/azure/cosmos-db/secure-access-to-data>
+
+User Management example with .NET SDK: <https://github.com/Azure/azure-cosmos-dotnet-v3/blob/master/Microsoft.Azure.Cosmos.Samples/Usage/UserManagement/UserManagementProgram.cs>
+
+Managing Users & Permissions in Cosmos DB with PowerShell: <https://dscottraynsford.wordpress.com/2018/01/14/managing-users-permissions-in-cosmosdb-with-powershell/>
---
+++ src/CosmosData.Tests/UserPermissions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CosmosData.Tests
+{
+    [TestClass]
+    public class UserPermissions
+    {
+        string _userWithReadPermissionToken;
+
+        [TestInitialize]
+        public async Task CreateUserWithReadPermission()
+        {
+            // based on https://github.com/Azure/azure-cosmos-dotnet-v3/blob/master/Microsoft.Azure.Cosmos.Samples/Usage/UserManagement/UserManagementProgram.cs
+
+            // load configuration from appsettings.json file
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            using (var client = new CosmosClient(config["CosmosData_ConnectionString"]))
+            {
+                var database = client.GetDatabase(config["CosmosData_DatabaseId"]);
+                var container = client.GetContainer(config["CosmosData_DatabaseId"], config["CosmosData_Item_Container"]);
+
+                // Create a user
+                User user1 = await database.UpsertUserAsync("readonly_user_test1");
+
+                // Create read permissions on the container
+                PermissionProperties readPermission = new PermissionProperties(
+                    id: "Read",
+                    permissionMode: PermissionMode.Read,
+                    container: container);
+
+                // Permissions token will be regenerated here with an expiry of 1 hour
+                PermissionProperties permission = await user1.UpsertPermissionAsync(readPermission);
+
+                // Save the permissions in a string variable
+                _userWithReadPermissionToken = permission.Token;
+            }
+        }
+
+        [TestMethod]
+        public async Task ConnectWithUserWithReadPermission()
+        {
+            // load configuration from appsettings.json file
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            // initialise Application Insights telemetry client
+            var insights = InsightsHelper.InitializeTelemetryClient(config["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+
+            // use the Resource token instead of a master access token
+            using (var client = new CosmosClient(config["CosmosData_AccountEndpoint"], _userWithReadPermissionToken))
+            {
+                // new instance of Data helper
+                ICosmosData<Item> data = new CosmosData<Item>(
+                    config,
+                    insights,
+                    client);
+
+                // Get all items
+                var getAllItems = await data.GetAll();
+            }
+        }
+    }
+}
